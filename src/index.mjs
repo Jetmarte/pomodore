8fffab924f3ed788b2c4979502e250b2dfac3519
Jetmarte <obcollazos@gmail.com>
2023-07-10 17:07:52 -0400

    se agregó modulo cors

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,16 @@
 import express, { Router, response } from 'express';
 import morgan from 'morgan';
 import {router} from './controller/routes/projectRoutes.mjs';
+import cors from "cors";
 
 const app = express();
-
+app.use(cors());
+app.use( function (req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-AllowOrigin", "GET,HEAD,POTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 //allow server manage html info
 app.use(express.urlencoded({extended: false}));
 //allow our server manage a json format.
